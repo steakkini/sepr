39ac1422b60decc7750d0f26bed23e63a98fd64d
Patrick Kienbauer <steakkini@users.noreply.github.com>
2015-05-14 09:25:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,17 +16,17 @@ node_server.use(bodyParser.urlencoded({
 node_server.post('/users', chess.signUp);
 node_server.put('/users/logIn/:userId', chess.logIn);
 node_server.put('/users/logOut/:userId', chess.logOut);
-node_server.delete('/users/delete/:userId', chess.deleteUser);
+node_server.delete('/users/:userId', chess.deleteUser);
 node_server.put('/users/forgot/:userId', chess.forgotPassword);
 node_server.put('/users/change/:userId', chess.changePassword);
 node_server.get('/users', chess.allUsers);
 node_server.get('/users/online', chess.allOnlineUsers);
 node_server.post('/matches', chess.newMatch);
-node_server.put('/matches/status/:matchId', chess.changeMatchStatus);
+node_server.put('/matches/:matchId', chess.changeMatchStatus);
 node_server.get('/matches', chess.getAllMatches);
 node_server.get('/matches/byId/:matchId', chess.getMatch);
 node_server.get('/matches/byUser/:userId', chess.getMatchByUser);
-node_server.post('/matches/move', chess.move);
+node_server.post('/matches/moves', chess.move);
 //node_server.post('/moves/pgnToMoves', chess.pgnToMoves);
 //node_server.post('/moves/movesToPgn/:matchId', chess.MovesToPgn);
 
